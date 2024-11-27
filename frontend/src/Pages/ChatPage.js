088eaa609ef68229afc805ff989fbfa88138573d
Javid Uddin <mjuddin021@gmail.com>
2024-11-27 13:34:37 -0600

    added NON REAL TIME messaging between registered clients, individual message history

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -11,9 +11,11 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 function ChatPage() {
     const { isSignedIn, user } = useUser();
     const [usersList, setUsersList] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState('');
     const [loading, setLoading] = useState(true);
+    const [sending, setSending] = useState(false);
     const [mode, setMode] = useState('friends');
 
     // fetch list of users from the server
@@ -22,8 +24,8 @@ function ChatPage() {
             .get('http://localhost:5001/api/get-clerk-users')
             .then((response) => {
                 const users = response.data.data;
-                console.log(users);
-                setUsersList(users);
+                const usersFiltered = users.filter(u => u.id !== user.id);
+                setUsersList(usersFiltered);
             })
             .catch((error) => {
                 console.error('Error fetching users:', error);
@@ -34,38 +36,43 @@ function ChatPage() {
     }, []);
 
     const fetchMessages = async () => {
-        if(!user) return;
+        if(!user || !selectedUser) return;
 
+        setLoading(true);
         const {data, error} = await supabase
             .from('Messages')
-            .select()
-            .or('from_user.eq.' + user.id + ',to_user.eq.' + user.id)
-
+            .select('*')
+            .or(
+                `and(from_user.eq.${user.id},to_user.eq.${selectedUser.id}),and(from_user.eq.${selectedUser.id},to_user.eq.${user.id})`
+            )
+            .order('time_sent', { ascending: true });
+        setLoading(false);
         if(error) {
             console.log("Error fetching messages: ", error);
         } else {
+            console.log("Messages: ", data);
             setMessages(data);
         }
     };
 
     const sendMessage = async () => {
-        if(!user || !message) return;
+        if(!user || !message || !selectedUser) return;
 
         const msgContent = message;
         setMessage('');
-        setLoading(true);
+        setSending(true);
         const { error } = await supabase
             .from('Messages')
             .insert([
                 {
                     from_user: user.id,
-                    to_user: user.id,
+                    to_user: selectedUser.id,
                     content: msgContent,
                     time_sent: new Date(),
                 },
             ]);
-        setLoading(false);
-        fetchMessages();
+        setSending(false);
+        await fetchMessages();
         if(error) {
             console.log("Error sending message: ", error);
         }
@@ -76,10 +83,22 @@ function ChatPage() {
     }
 
     useEffect(() => {
-        if(isSignedIn) {
+        if (selectedUser) {
             fetchMessages();
         }
-    }, [isSignedIn, user?.id]);
+    }, [selectedUser]);
+
+    function changeSelectedUser(user) {
+        if(user !== selectedUser) {
+            setMessages([]);
+            setSelectedUser(user);
+        }
+        if(user) {
+            toggleMode('chat');
+        } else {
+            toggleMode('friends');
+        }
+    }
 
     if (!isSignedIn) {
         return (
@@ -93,54 +112,57 @@ function ChatPage() {
         <div className="ChatPage">
             <SignOutButton className="signOutBtn"/>
             <div className="panel">
-                <p className={`panelItem ${mode === 'friends' ? 'selected' : ''}`} onClick={(e) => toggleMode('friends')}>Friends</p>
-                <p className={`panelItem ${mode === 'chat' ? 'selected' : ''}`} onClick={(e) => toggleMode('chat')}>Chat</p>
+                <p className={`panelItem ${mode === 'friends' && selectedUser ? 'selected' : ''}`} onClick={(e) => toggleMode('friends')}>Friends</p>
+                {selectedUser && (<p className={`panelItem ${mode === 'chat' ? 'selected' : ''}`} onClick={(e) => toggleMode('chat')}>Chat</p>)}
             </div>
             {mode === 'friends' && (
                 <div className="userListPanel">
                     <strong>Supachat Friends</strong>
+                    <div className="addFriend">Add friend</div>
                     <div className="users">
                         {usersList.length === 0 ? (
-                            <p>No users available.</p>
+                            <p>No friends available.</p>
                         ) : (
-                            usersList.map((user, index) => (
-                                <p key={user.id} className="username">{user.firstName}</p>
+                            usersList.map((currUser, index) => (
+                                <p key={currUser.id} className="username" onClick={(e) => changeSelectedUser(currUser)}>{currUser.firstName} <span className="tag">@{currUser.username}</span></p>
                             ))
                         )}
                     </div>
                 </div>
             )}
-            {mode === 'chat' && (
+            {mode === 'chat' && selectedUser && (
                 <div className="messageContainer">
-                    <div className="chattingWith">Chat with NAME</div>
+                    <div className="closeChat" onClick={(e) => changeSelectedUser(null)}>✖</div>
+                    <div className="chattingWith">Chat with {selectedUser.firstName} <span className="tag">@{selectedUser.username}</span></div>
                     <div className="messages">
-                        {messages.length === 0 ? (
-                            <p>No messages available.</p>
+                        {loading && (
+                            <p>Loading message history...</p>
+                        )}
+                        {!loading && messages.length === 0 ? (
+                            <p>No message history available.</p>
                         ) : (
                             messages.map((msg, index) => (
                                 <div key={index} className={`message ${msg.from_user === user.id ? 'you' : ''}`}>
                                     <p className="content">{msg.content}</p>
-                                    {msg.from_user !== user.id && (
-                                        <p className="user">Someone Else</p>
-                                    )}
                                 </div>
                             ))
                         )}
                     </div>
                     <div className="messageBox">
-                    <textarea
-                        id="txtArea"
-                        rows="1"
-                        placeholder={loading ? "Sending..." : "Message"}
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        onKeyDown={(e) => {
-                            if (e.key === 'Enter' && !e.shiftKey) {
-                                e.preventDefault();
-                                sendMessage();
-                            }
-                        }}
-                    />
+                        <textarea
+                            autoFocus
+                            id="txtArea"
+                            rows="1"
+                            placeholder={sending ? "Sending..." : "Message"}
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter' && !e.shiftKey) {
+                                    e.preventDefault();
+                                    sendMessage();
+                                }
+                            }}
+                        />
                         <div className={`messageBtn ${message ? 'visible' : ''}`} onClick={sendMessage}>Send</div>
                     </div>
                 </div>
